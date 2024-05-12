2f2d7e8a766472e4fa4a294202e6ba17a91436f0
KISEE41 <shreejan.kisee005@gmail.com>
2024-05-12 11:43:59 +0545

    add: pipeline code, and code for training the model

--- model_training/train_test_split.py
+++ model_training/train_test_split.py
@@ -19,10 +19,12 @@ def get_train_test_score_set(df: pd.DataFrame) -> Tuple:
     """
     # train test set
     x_train, x_test, y_train, y_test = train_test_split(
-        df.drop("target", axis=1), df["target"], test_size=0.2, random_state=42
+        df.drop("target", axis=1), df["target"], test_size=0.4, random_state=42
     )
 
     # test validation set
-    x_test, x_val, y_test, y_val = train_test_split(x_test, y_test, test_size=0.5, random_state=42)
+    x_test, x_val, y_test, y_val = train_test_split(
+        x_test, y_test, test_size=0.5, random_state=42
+    )
 
     return x_train, x_test, x_val, y_train, y_test, y_val
